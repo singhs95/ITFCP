9a68a79ba5926f3f028c8b7cdfc73ba0680da4b2
singhs95 <gurdinho10@gmail.com>
2022-02-16 11:58:24 +0100

    Initial Commit

---
+++ ITFCP/ITFCPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITFCPApp.swift
+//  ITFCP
+//
+//  Created by Gursharnbir Singh on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ITFCPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
